ce7c4a3f5eaf98a6139d5a0a64cf00fd0581db25
Matheus <matheus.santiago@sances.com.br>
2021-03-26 17:45:58 -0300

    Adicionando helper-http e interface de http-response

---
+++ app/presentation/helpers/http-helper.ts
@@ -0,0 +1,21 @@
+import { HttpResponse } from '../protocols/http'
+
+export const ok = (data: any): HttpResponse => ({
+    body: data,
+    statusCode: 200
+})
+
+export const created = (data: any): HttpResponse => ({
+    body: data,
+    statusCode: 201
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+    body: error,
+    statusCode: 400
+})
+
+export const forbidden = (data: any): HttpResponse => ({
+    body: data,
+    statusCode: 403
+})
