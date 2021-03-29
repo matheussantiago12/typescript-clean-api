3b4b6c46c9a2de14891f1034794f543f1152033b
Matheus <matheus.santiago@sances.com.br>
2021-03-29 15:45:51 -0300

    Removendo usecase editar post

--- app/data/protocols/database/post/edit-post-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Post } from '@app/domain/entities/post'
-import { EditPostParams } from '@app/domain/usecases/post/edit-post'
-
-interface EditPostRepository {
-    create: (data: EditPostParams) => Promise<Post>
-}
-
-export { EditPostRepository, EditPostParams }
--- app/domain/usecases/post/edit-post.ts
+++
@@ -1,9 +0,0 @@
-import Post from '@app/domain/entities/post'
-
-type EditPostParams = Post
-
-interface EditPost {
-    edit: (id: number, data: EditPostParams) => Promise<Post>
-}
-
-export { EditPostParams, EditPost }
