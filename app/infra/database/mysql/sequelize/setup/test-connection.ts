238fd74c20eb269d3f44077205e07a986d1464bf
Matheus <matheus.santiago@sances.com.br>
2021-03-29 08:30:14 -0300

    Criando arquivo separado para testar conexão com o bando de dados

---
+++ app/infra/database/mysql/sequelize/setup/test-connection.ts
@@ -0,0 +1,12 @@
+import { sequelize } from './index'
+
+const testConnection = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log('Connection has been established successfully.')
+    } catch (error) {
+        console.error('Unable to connect to the database:', error)
+    }
+}
+
+testConnection()
