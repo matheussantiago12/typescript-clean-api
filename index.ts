00628c1d21d04b68085fb31134ce52e1bd3d715f
Matheus <matheus.santiago@sances.com.br>
2021-03-29 11:58:37 -0300

    Adicionando rotas ao index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import routes from '@app/main/routes'
 
 const app = express()
 const PORT = 8000
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'))
+app.get('/', (req, res) => res.send('Typescript clean API'))
+app.use(express.json())
+app.use(routes)
 app.listen(PORT, () => {
     console.log(`Server is running at https://localhost:${PORT}`)
 })
