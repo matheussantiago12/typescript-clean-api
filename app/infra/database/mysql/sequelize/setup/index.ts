2c8db7941c39b12c4843a7ac1abceb11d034ad0f
Matheus <matheus.santiago@sances.com.br>
2021-03-31 13:35:50 -0300

    Alterando parseInt para Number

--- app/infra/database/mysql/sequelize/setup/index.ts
+++ app/infra/database/mysql/sequelize/setup/index.ts
@@ -3,6 +3,6 @@ import config from '@app/infra/database/mysql/config'
 
 export const sequelize = new Sequelize(config.DB!, config.USER!, config.PASSWORD!, {
     host: config.HOST,
-    port: parseInt(config.PORT!),
+    port: Number(config.PORT!),
     dialect: 'mysql'
 })
