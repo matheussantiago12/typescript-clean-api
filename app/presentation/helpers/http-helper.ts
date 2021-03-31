3a9b52eea6a8db5f8516b91cbb0e018d48e7a7aa
Matheus <matheus.santiago@sances.com.br>
2021-03-31 10:15:16 -0300

    Removendo http-responses não utilizadas

--- app/presentation/helpers/http-helper.ts
+++ app/presentation/helpers/http-helper.ts
@@ -1,21 +1,6 @@
 import { HttpResponse } from '@app/presentation/protocols/http'
 
-export const ok = (data: any): HttpResponse => ({
-    body: data,
-    statusCode: 200
-})
-
 export const created = (data: any): HttpResponse => ({
     body: data,
     statusCode: 201
-})
-
-export const badRequest = (error: Error): HttpResponse => ({
-    body: error,
-    statusCode: 400
-})
-
-export const forbidden = (data: any): HttpResponse => ({
-    body: data,
-    statusCode: 403
 })
