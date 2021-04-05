68313a96ddcb3e95f198c629e2b3e5722f647a1e
Matheus <matheus.santiago@sances.com.br>
2021-04-05 11:39:15 -0300

    Implementando swagger

--- app/main/config/app.ts
+++ app/main/config/app.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import routes from '@app/main/routes'
+import swaggerUi from 'swagger-ui-express'
+// @ts-ignore
+import swaggerJson from '@/swagger.json'
 
 const app = express()
 
 app.get('/', (req, res) => res.send('Typescript clean API'))
 app.use(express.json())
 app.use(routes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson))
 
 export default app
